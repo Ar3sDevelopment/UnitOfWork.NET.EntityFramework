8e99f242da241c982a816840940ee94087c4f90f
Matteo Bruni <ar3s@icloud.com>
2016-04-27 17:23:18 +0200

    nunit creation

---
+++ UnitOfWork.NET.EntityFramework.NUnit/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+namespace UnitOfWork.NET.EntityFramework.NUnit
+{
+	[TestFixture()]
+	public class Test
+	{
+		[Test()]
+		public void TestCase()
+		{
+		}
+	}
+}
+
