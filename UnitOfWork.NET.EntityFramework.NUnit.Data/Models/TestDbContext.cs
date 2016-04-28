bcd7eabf8d44f4be50600a10cdc2064320172416
Matteo Bruni <ar3s@icloud.com>
2016-04-28 10:15:45 +0200

    trying fixing build

--- UnitOfWork.NET.EntityFramework.NUnit.Data/Models/TestDbContext.cs
+++ UnitOfWork.NET.EntityFramework.NUnit.Data/Models/TestDbContext.cs
@@ -1,29 +1,31 @@
 using System.Data.Entity;
 using UnitOfWork.NET.EntityFramework.NUnit.Data.Models.Mapping;
+using System.Runtime.CompilerServices;
 
 namespace UnitOfWork.NET.EntityFramework.NUnit.Data.Models
 {
-    public partial class TestDbContext : DbContext
-    {
-        static TestDbContext()
-        {
-            Database.SetInitializer<TestDbContext>(null);
-        }
+	public partial class TestDbContext : DbContext
+	{
+		static TestDbContext()
+		{
+			Database.SetInitializer(new CreateDatabaseIfNotExists<TestDbContext>());
+			Database.SetInitializer(new MigrateDatabaseToLatestVersion<TestDbContext, Configuration>());
+		}
 
-        public TestDbContext()
-            : base("Name=TestDbContext")
-        {
-        }
+		public TestDbContext()
+			: base("Name=TestDbContext")
+		{
+		}
 
-        public DbSet<Role> Roles { get; set; }
-        public DbSet<User> Users { get; set; }
-        public DbSet<UserRole> UserRoles { get; set; }
+		public DbSet<Role> Roles { get; set; }
+		public DbSet<User> Users { get; set; }
+		public DbSet<UserRole> UserRoles { get; set; }
 
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            modelBuilder.Configurations.Add(new RoleMap());
-            modelBuilder.Configurations.Add(new UserMap());
-            modelBuilder.Configurations.Add(new UserRoleMap());
-        }
-    }
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			modelBuilder.Configurations.Add(new RoleMap());
+			modelBuilder.Configurations.Add(new UserMap());
+			modelBuilder.Configurations.Add(new UserRoleMap());
+		}
+	}
 }
