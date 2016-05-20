e80583d7e3ad863a714f4996bcc5a27f172bb1b7
Matteo Bruni <ar3s@icloud.com>
2016-05-20 11:26:41 +0200

    test updated

--- UnitOfWork.NET.EntityFramework.NUnit/Classes/UnitOfWork.cs
+++ UnitOfWork.NET.EntityFramework.NUnit/Classes/UnitOfWork.cs
@@ -1,11 +1,40 @@
 ﻿using UnitOfWork.NET.EntityFramework.Classes;
 using UnitOfWork.NET.EntityFramework.NUnit.Data.Models;
 using UnitOfWork.NET.EntityFramework.NUnit.Repositories;
+using System;
 
 namespace UnitOfWork.NET.EntityFramework.NUnit.Classes
 {
-    public class TestUnitOfWork : EntityUnitOfWork<TestDbContext>
-    {
-        public UserRepository Users { get; set; }
-    }
+	public class TestUnitOfWork : EntityUnitOfWork<TestDbContext>
+	{
+		public UserRepository Users { get; set; }
+
+		public override void AfterSaveChanges(System.Data.Entity.DbContext context)
+		{
+			base.AfterSaveChanges(context);
+
+			Console.WriteLine("DbContext After SaveChanges");
+		}
+
+		public override void AfterSaveChanges(TestDbContext context)
+		{
+			base.AfterSaveChanges(context);
+
+			Console.WriteLine("TestDbContext After SaveChanges");
+		}
+
+		public override void BeforeSaveChanges(System.Data.Entity.DbContext context)
+		{
+			base.BeforeSaveChanges(context);
+
+			Console.WriteLine("DbContext Before SaveChanges");
+		}
+
+		public override void BeforeSaveChanges(TestDbContext context)
+		{
+			base.BeforeSaveChanges(context);
+
+			Console.WriteLine("TestDbContext Before SaveChanges");
+		}
+	}
 }
