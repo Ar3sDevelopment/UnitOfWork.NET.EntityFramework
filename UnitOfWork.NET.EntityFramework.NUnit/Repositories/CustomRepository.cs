7faf018fa8d90d83162bab3b449c70f8d3a17425
Matteo Bruni <ar3s@icloud.com>
2016-09-30 14:05:05 +0200

    try fix

---
+++ UnitOfWork.NET.EntityFramework.NUnit/Repositories/CustomRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnitOfWork.NET.EntityFramework.Classes;
+using UnitOfWork.NET.Interfaces;
+namespace UnitOfWork.NET.EntityFramework.NUnit.Repositories
+{
+	public abstract class CustomRepository<TEntity, TDTO> : EntityRepository<TEntity, TDTO> where TEntity : class, new() where TDTO : class, new()
+	{
+		protected CustomRepository(IUnitOfWork manager) : base(manager)
+		{
+		}
+	}
+}
